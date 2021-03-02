f7ffde13339fa3ab850d4a9552ac2c15f42227b6
Ricardo Fontenla <ric_parada@hotmail.com>
2021-03-01 22:16:48 -0300

    primeiro commit

---
+++ part_1/binary_boarding.rb
@@ -0,0 +1,20 @@
+# The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the plane (numbered 0 through 127).
+# Each letter tells you which half of a region the given seat is in. Start with the whole list of rows;
+# the first letter indicates whether the seat is in the front (0 through 63) or the back (64 through 127).
+# The next letter indicates which half of that region the seat is in, and so on until you're left with exactly one row
+# The last three characters will be either L or R; these specify exactly one of the 8 columns of seats on the plane (numbered 0 through 7).
+# The same process as above proceeds again, this time with only three steps. L means to keep the lower half, while R means to keep the upper half
+# Every seat also has a unique seat ID: multiply the row by 8, then add the column
+# As a sanity check, look through your list of boarding passes. What is the highest seat ID on a boarding pass?
+
+require 'csv'
+require_relative './lib/passes_scan.rb'
+
+puts('Binary Boarding')
+
+file = File.read('./../data/boarding_passes.txt')
+csv = CSV.parse(file)
+
+passes = PassesScan.new(csv)
+
+puts("O maior ID de um assento é: #{passes.highest_id}")
---
+++ part_2/lib/passes_scan.rb
@@ -0,0 +1,104 @@
+class PassesScan
+
+  #atributos
+  attr_reader :csv, :passes_arrey
+
+  #construtor
+  def initialize(csv)
+    @csv = csv
+    @passes_arrey = scan()
+  end
+
+  #metodos
+  def highest_id()
+    h_id = 0
+    @passes_arrey.each do |hash|
+      if hash[:ID].to_i > h_id 
+        h_id = hash[:ID].to_i
+      end       
+    end
+    return h_id
+  end
+  
+  def lowest_id()
+    l_id = highest_id()
+    @passes_arrey.each do |hash|
+      if hash[:ID].to_i < l_id 
+        l_id = hash[:ID].to_i
+      end       
+    end
+    return l_id
+  end
+
+  def missing_id
+    sorted = id_sorter()
+    i = 1
+    sorted.each do |pass|
+      if (pass[:ID] + 1 != sorted[i][:ID])
+        return (pass[:ID] + 1)
+      else
+        i += 1
+      end
+    end
+    
+  end
+
+  #metodos privados
+  private
+
+  def scan()
+    passes_hash = []
+    csv.each do |array|
+      i = 0
+      rows = (0..127).to_a
+      new_rows = []
+      while i < 7 do
+        pass = array[0]
+        letter = pass[i]
+        if letter == 'B'
+          new_rows = rows.each_slice(rows.size/2).to_a
+          rows = new_rows[1]
+        else
+          new_rows = rows.each_slice(rows.size/2).to_a
+          rows = new_rows[0]
+        end
+        new_rows = []
+        i += 1
+      end
+      colum = colum_finder(array)
+      id = (rows[0] * 8) + colum[0]
+      #puts ("A passagem #{array[0]} corresponde a ROW: #{rows[0]} coluna: #{colum[0]} e ID: #{id}")
+      passes_hash << {pass: array[0], row: rows[0], colum: colum[0] ,ID: id}
+    end
+    return passes_hash
+  end
+
+  def colum_finder(array)
+      j = 7
+    columns = (0..7).to_a
+    new_columns = []
+    
+    while j < 10 do
+      pass = array[0]
+      letter = pass[j]
+    if letter == 'R'
+      new_columns = columns.each_slice(columns.size/2).to_a
+      columns = new_columns[1]
+    else
+      new_columns = columns.each_slice(columns.size/2).to_a
+      columns = new_columns[0]
+    end
+    pass = ''
+      new_columns = []
+      j += 1
+    end
+    colum = columns
+    return colum
+  end
+
+  def id_sorter()
+    sorted_ids = @passes_arrey.sort_by! { |h| h[:ID] }
+    return sorted_ids
+  end
+  
+end
